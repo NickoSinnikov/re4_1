7ef96883597f01e51cee08eba75f1a41ae75b7aa
Nicko Sinnikov <nickosinnikov@outlook.com>
2022-12-20 00:28:56 +0300

    first commit

---
+++ src/components/ChangeColorForm.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+//import { Component } from 'react';
+
+export default function ChangeColorForm() {
+  const [form, setForm] = useState('');
+  const [rgbColor, setRgbColor] = useState('');
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setForm(value);
+
+    let rgb = '';
+    if (value.length === 7) {
+      console.log(value);
+      const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(value);
+      console.log(result);
+      rgb = result
+        ? `rgb(${parseInt(result[1], 16)},
+      ${parseInt(result[2], 16)},
+      ${parseInt(result[3], 16)})`
+        : 'Ошибка!';
+    } else if (value.length > 7) {
+      rgb = 'Ошибка!';
+    }
+
+    setRgbColor(rgb);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  //console.log(this.colorBg);
+  return (
+    <div
+      className="container"
+      style={
+        rgbColor === 'Ошибка!'
+          ? { backgroundColor: 'red' }
+          : { backgroundColor: rgbColor }
+      }
+    >
+      <form onSubmit={handleSubmit}>
+        <label>Введите цвет в формате HEX</label>
+        <input type="text" value={form} onChange={handleChange} />
+      </form>
+      <div className="rgbField">{rgbColor}</div>
+    </div>
+  );
+}
